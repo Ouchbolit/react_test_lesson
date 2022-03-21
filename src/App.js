8b2dbf6c3ccf313d3b6919255cf9f46f1e87ae74
Ouchbolit <selim.bazarov@gmail.com>
2022-03-21 22:24:27 +0300

    render test tasks data

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import './App.css';
 import MainPage from "./components/MainPage";
+import {TASK_TEST_LIST} from "./constants";
+import {useEffect, useState} from "react";
 
-function App() {
-  // tasks list
-  // deadline
-  // assign to user
-  // ask info
-  // task completed
-  // edit task
-  // create and delete tasks
-  return (
-    <div className="App">
-      <MainPage/>
-    </div>
-  );
+const App = () => {
+
+    // tasks list
+    // deadline
+    // assign to user
+    // ask info
+    // task completed
+    // edit task
+    // create and delete tasks
+
+    const [tasks, setTasks] = useState([])
+
+    useEffect(() => {
+        setTasks(TASK_TEST_LIST)
+    }, [])
+
+
+    return (
+        <div className="App">
+            <MainPage tasks={tasks}/>
+        </div>
+    );
 }
 
 export default App;
--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,12 +1,21 @@
 import React from "react";
 import classes from './mainPage.module.css'
 
-const MainPage = () => {
+const MainPage = (props) => {
     return (
-        <div> className={classes.mainPageWrapper}>
-            <div>
+        <div className={classes.mainPageWrapper}>
 
-            </div>
+            {props.tasks.map((task) => (
+                    <div className={classes.taskCard}>
+                        <div>{task.title}</div>
+                        <div>{task.description}</div>
+                        <div>20.03.2022 17:00</div>
+                    </div>
+                )
+            )}
+
+            {/*{JSON.stringify(props)}*/}
+
         </div>
     )
 }
