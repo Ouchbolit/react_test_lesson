fa4914d78b96e011a6f7cd84100c24f555629108
Ouchbolit <selim.bazarov@gmail.com>
2022-03-21 18:55:03 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import MainPage from "./components/MainPage";
+
+function App() {
+  // tasks list
+  // deadline
+  // assign to user
+  // ask info
+  // task completed
+  // edit task
+  // create and delete tasks
+  return (
+    <div className="App">
+      <MainPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MainPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import classes from './mainPage.module.css'
+
+const MainPage = () => {
+    return (
+        <div> className={classes.mainPageWrapper}>
+            <div>
+
+            </div>
+        </div>
+    )
+}
+
+export default MainPage
